3d9c4c9cfe5c061e8b52bb8e2d5cb81391e6b31e
bvenners <bill@artima.com>
2016-12-08 11:35:16 +0000

    Added variants of squareRoot that use ensuring. Numbering is now bad.

--- src/main/scala/org/scalatest/examples/Demo.scala
+++ src/main/scala/org/scalatest/examples/Demo.scala
@@ -4,6 +4,21 @@ import org.scalactic.anyvals.PosZDouble
 
 object Demo {
 
+  def squareRoot0(x: Double): Double = {
+    require(x >= 0.0 && !x.isPosInfinity)
+    math.sqrt(x) ensuring { res =>
+      res == math.sqrt(x)
+    }
+  }
+
+  def squareRoot00(x: Double): Double = {
+    require(x >= 0.0 && !x.isPosInfinity)
+    math.sqrt(x) ensuring { res =>
+      val tolerance = math.ulp(x) // Unit in the last place
+      (res * res - x).abs <= tolerance
+    }
+  }
+
   def squareRoot1(x: Double): Double = {
     require(x >= 0.0 && !x.isPosInfinity)
     math.sqrt(x)
